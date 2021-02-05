8c3d145a5da5b556ee2e7162cd14bbb3fda466fc
Hadi <hadan326@student.liu.se>
2021-02-05 21:05:24 +0100

    Simple version of proxy (no content-modification)

--- proxy.py
+++ proxy.py
@@ -8,28 +8,26 @@ server = '127.0.1.1'
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind((server,serverPort))
 serverSocket.listen(1)
-print('The server is ready to receive')
+print('The server is ready to receive...\n{0}\n'.format("="*40))
 
 while True:
     connectionSocket, addr = serverSocket.accept()
     # print("Client socket: ", connectionSocket)
     # print("Client address: ", addr)
     sentence = connectionSocket.recv(1024).decode('unicode_escape')
-    match = re.search(r"Host: (.+)", sentence)
-    destinationIP = match.group(1)[0:-1]
-    print(len(destinationIP))
-    print(destinationIP)
-    #capitalizedSentence = sentence.upper()
     
+    
+    match = re.search(r"Host: (.+[a-z]+)", sentence)
+    destinationIP = match.group(1)
+    print("Host is:\t{0}\nLength of hostname is:\t{1}\n\nMessage is:\n{2}".format(destinationIP, len(destinationIP), sentence))
+    #capitalizedSentence = sentence.upper()
 
     clientSideSocket = socket(AF_INET, SOCK_STREAM)
     clientSideSocket.connect((destinationIP, DESTINATION_PORT))
     clientSideSocket.send(sentence.encode())
-    
     modifiedSentence = clientSideSocket.recv(1024)
+    print("Response is:\t{0}\n{1}".format(modifiedSentence.decode('unicode_escape'), "="*40))
+    connectionSocket.send(modifiedSentence)
+    
     clientSideSocket.close()
-    print(modifiedSentence.decode('unicode_escape'))
-    #print(sentence)
-    #connectionSocket.send(capitalizedSentence.encode())
-    #connectionSocket.send("Hello World!".encode())
     connectionSocket.close()
