df1e046267fa45056b030fb6587b30cc149f3da2
Hadi <hadan326@student.liu.se>
2021-03-06 18:03:52 +0100

    Poison reverse implemented (first version)

--- Assignment4/RouterNode.py
+++ Assignment4/RouterNode.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from colorama import Fore, Back, Style
 import GuiTextArea, RouterPacket, F
 from copy import deepcopy
 
@@ -20,10 +21,11 @@ class RouterNode():
         self.costs = deepcopy(costs)
         self.orgCosts = deepcopy(costs)
         self.neighborsCosts = {} # EX: {1:4, 2:1} c(x,y)
-       # self.neighbors = []
         self.distanceVectors = {self.myID: self.costs}
         self.nextRouter = {}
         self.routingTable = []
+
+        self.condition = False
         
         print("Costs is:" , self.costs)
 
@@ -35,15 +37,6 @@ class RouterNode():
                         self.neighborsCosts[j] = self.sim.connectcosts[i][j]
                 break
             
-
-        # Finding directly attached neighbors
-        # for i in range(len(self.sim.connectcosts)):
-        #     if i == self.myID:
-        #         for j in range(len(self.sim.connectcosts)):
-        #             if self.sim.connectcosts[i][j] != sim.INFINITY and self.sim.connectcosts[i][j] != 0:
-        #                 self.neighbors.append(j)
-        #         break
-            
         # Building distanceVectors containing my and my neighbors distance vector
         for neighbor in self.neighborsCosts:
             self.distanceVectors[neighbor] = []
@@ -52,20 +45,6 @@ class RouterNode():
             
         print(self.distanceVectors)
 
-        # Building routing table
-        # for i in range(self.sim.NUM_NODES):
-        #     if i == self.myID:
-        #         self.routingTable.append(self.costs)
-        #         continue
-        #     temp_list = []
-        #     for j in range(self.sim.NUM_NODES):
-        #         temp_list.append(self.sim.INFINITY)
-
-        #     self.routingTable.append(temp_list)
-            
-        # print("This is routingTable for router {}".format(self.myID))
-        # print(self.routingTable)
-        
         for i in range(self.sim.NUM_NODES):
             if i in self.neighborsCosts and i != self.myID:
                 #print("{} Finns i {}.".format(i, self.neighborsCosts))
@@ -82,8 +61,7 @@ class RouterNode():
         
     # --------------------------------------------------
     def recvUpdate(self, pkt):
-        print("recvUpdate called from router {} from router {}!".format(self.myID, pkt.sourceid))
-
+        print("Router {} is receiving {} from router {}".format(self.myID, pkt.mincost, pkt.sourceid))
         self.distanceVectors[pkt.sourceid] = pkt.mincost
         self.calculate()
 
@@ -136,47 +114,21 @@ class RouterNode():
        
     # --------------------------------------------------
     def updateLinkCost(self, dest, newcost):
-        # print("updateLink called from router {}".format(self.myID))
+
+        print("updateLink called from router {}".format(self.myID))
         print(dest, newcost)
-
-        self.costs[dest] = self.sim.INFINITY
-        print(self.costs)
+        print(Fore.GREEN+ 'Here') 
         self.neighborsCosts[dest] = newcost
+        print(self.costs)
 
-        for n in self.neighborsCosts:
-            pkt = RouterPacket.RouterPacket(self.myID, n, deepcopy(self.costs))
-            print("Sending distance vector from router {} to router {}".format(self.myID, n))
-            self.sendUpdate(pkt)
+        if self.sim.POISONREVERSE:
+             for neighbor in self.neighborsCosts:
+                 if neighbor != dest and self.sim.nodes[neighbor].nextRouter[dest] == self.myID:
+                     self.distanceVectors[neighbor][dest] = self.sim.INFINITY
 
-
-    # --------------------------------------------------
-    # Works without linkcost-change.         
-    # def calculate(self):
-    #     print("Distance vectors in router {} is.".format(self.myID))
-    #     for i in sorted(self.distanceVectors):
-    #         print( str(i) + " : " + str(self.distanceVectors[i]))
-
-    #     print("NeighborsCosts is: " , self.neighborsCosts)
-    #     changed = False
-    #     for n in range(self.sim.NUM_NODES):
-    #         mincost = self.costs[n] # 10
-    #         for neighbor in self.neighborsCosts:
-    #             if self.neighborsCosts[neighbor] + self.distanceVectors[neighbor][n] < mincost:
-    #                 changed = True
-    #                 mincost = self.neighborsCosts[neighbor] + self.distanceVectors[neighbor][n]
-    #                 self.nextRouter[n] = neighbor
-
-    #         if changed:
-    #             self.costs[n] = mincost
-
-    #     if changed:
-    #         print("Distance vector changed for router {} and now is {}.".format(self.myID, self.costs))
-    #         for n in self.neighborsCosts:
-    #             pkt = RouterPacket.RouterPacket(self.myID, n, deepcopy(self.costs))
-    #             print("Sending distance vector from router {} to router {}".format(self.myID, n))
-    #             self.sendUpdate(pkt)
-    #     else:
-    #         print("Distance vector DID NOT change for router {}.".format(self.myID))
+        print(self.distanceVectors)
+        self.calculate()
+        
     # --------------------------------------------------
     # New version. Works with linkcost-change on
     def calculate(self):
@@ -189,6 +141,9 @@ class RouterNode():
         for n in range(self.sim.NUM_NODES):
             if n == self.myID:
                 continue
+
+         
+
             mincost = self.sim.INFINITY
             for neighbor in self.neighborsCosts:
                 if self.neighborsCosts[neighbor] + self.distanceVectors[neighbor][n] < mincost:
@@ -198,6 +153,20 @@ class RouterNode():
             if self.costs[n] != mincost:
                 self.costs[n] = mincost
                 changed = True
+
+            # if self.sim.POISONREVERSE:
+            #    # mincost = self.sim.INFINITY
+            #     for neighbor in self.neighborsCosts:
+            #         # send a white lie to those neighbors that has path to
+            #         # dest through myself
+            #         if self.nextRouter[n] == neighbor and neighbor != n:
+            #             print("I am running")
+            #             temp_costs = deepcopy(self.costs)
+            #             temp_costs[n] = self.sim.INFINITY
+            #             pkt = RouterPacket.RouterPacket(self.myID, neighbor, deepcopy(temp_costs))
+            #             self.sendUpdate(pkt)
+            #             break
+            #     continue
 
         if changed:
             print("Distance vector changed for router {} and now is {}.".format(self.myID, self.costs))
